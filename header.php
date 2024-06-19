9117f8f27f8e0fa0d1b899253093190b553e4ea8
Danial Zainal <danial7101@gmail.com>
2024-06-19 21:12:08 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav id="headernav">
+        <p>Danial Zainal</p>
+        <ul>
+            <li><a href="index.php" class="btn btn6">Home</a></li>
+            <li><a href="#about" class="btn btn6">About</a></li>
+            <li><a href="#projects" class="btn btn6">Projects</a></li>
+            <li><a href="#resume" class="btn btn6">Resume</a></li>
+        </ul>
+        <p>|&nbsp&nbsp<span>&#9993;</span> <a href="mailto:danial7101@gmail.com" style="text-decoration: none; color:white;">&nbspdanial7101@gmail.com</p></a>
+        
+    </nav>
+    
+</header>
---
+++ homepage.js
@@ -0,0 +1,51 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a[href^="#"]');
+    
+    for (const link of links) {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            
+            const targetId = link.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+});
+
+        document.getElementById('toggleButton').addEventListener('click', function() {
+            var nav = document.querySelector('#headernav ul');
+            nav.classList.toggle('show');
+        });
+
+    let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        header.classList.add('hidden');
+    } else {
+        // Scrolling up
+        header.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop;
+});
+
+// Toggle button functionality for mobile view
+const toggleButton = document.getElementById('toggleButton');
+const navUl = document.querySelector('#headernav ul');
+
+toggleButton.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
+
